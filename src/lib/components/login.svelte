af4b9e5da2fcf51d658da77f1a791e6f2383ae97
Visily <36494994+VisilyRomani@users.noreply.github.com>
2023-11-22 18:10:52 -0600

    initial setup

---
+++ src/lib/components/login.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { currentUser, pb } from '../pocketbase';
+	let username: string;
+	let password: string;
+
+	const login = async () => {
+		await pb.collection('users').authWithPassword(username, password);
+	};
+</script>
+
+{#if !currentUser}
+	<form on:submit|preventDefault>
+		<input bind:value={username} placeholder="username" />
+		<input bind:value={password} placeholder="password" />
+	</form>
+{/if}
