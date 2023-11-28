959bf08f87a927f966c31087e793df8679812d4b
Visily <36494994+VisilyRomani@users.noreply.github.com>
2023-11-27 21:32:40 -0600

    added if statements in front on modal

--- src/lib/components/modal/DebtModal.svelte
+++ src/lib/components/modal/DebtModal.svelte
@@ -26,58 +26,60 @@
 	});
 </script>
 
-<Modal bind:showModal={newDebtModal}>
-	<div>
-		<h3>Add New Debt</h3>
-		<form class="debt-container" use:enhance method="post" action="/dashboard?/newDebt">
-			{#if $errors.debt_to}
-				<p class="error-response">{$errors.debt_to}</p>
-			{/if}
-			<input type="hidden" name="debt_to" />
-			<div>
-				<input
-					placeholder="Description"
-					autocomplete="off"
-					name="description"
-					style={$errors.description && 'border:2px solid rgb(232, 174, 174)'}
-					bind:value={$form.description}
-				/>
-				{#if $errors.description}
-					<p class="error-response">{$errors.description}</p>
+{#if newDebtModal}
+	<Modal bind:showModal={newDebtModal}>
+		<div>
+			<h3>Add New Debt</h3>
+			<form class="debt-container" use:enhance method="post" action="/dashboard?/newDebt">
+				{#if $errors.debt_to}
+					<p class="error-response">{$errors.debt_to}</p>
 				{/if}
-			</div>
-			<div>
-				<select
-					name="debt_from"
-					bind:value={$form.debt_from}
-					style={$errors.debt_from && 'border:2px solid rgb(232, 174, 174)'}
-				>
-					<option value="" selected disabled hidden>Select Friend</option>
-					{#each friends as friend}
-						<option value={friend.id}>{friend.name}</option>
-					{/each}
-				</select>
-				{#if $errors.debt_from}
-					<p class="error-response">{$errors.debt_from}</p>
-				{/if}
-			</div>
-			<div>
-				<input
-					name="cost"
-					type="number"
-					step="0.01"
-					placeholder="Amount Owed"
-					bind:value={$form.cost}
-					style={$errors.cost && 'border:2px solid rgb(232, 174, 174)'}
-				/>
-				{#if $errors.cost}
-					<p class="error-response">{$errors.cost}</p>
-				{/if}
-			</div>
-			<button>Submit</button>
-		</form>
-	</div>
-</Modal>
+				<input type="hidden" name="debt_to" />
+				<div>
+					<input
+						placeholder="Description"
+						autocomplete="off"
+						name="description"
+						style={$errors.description && 'border:2px solid rgb(232, 174, 174)'}
+						bind:value={$form.description}
+					/>
+					{#if $errors.description}
+						<p class="error-response">{$errors.description}</p>
+					{/if}
+				</div>
+				<div>
+					<select
+						name="debt_from"
+						bind:value={$form.debt_from}
+						style={$errors.debt_from && 'border:2px solid rgb(232, 174, 174)'}
+					>
+						<option value="" selected disabled hidden>Select Friend</option>
+						{#each friends as friend}
+							<option value={friend.id}>{friend.name}</option>
+						{/each}
+					</select>
+					{#if $errors.debt_from}
+						<p class="error-response">{$errors.debt_from}</p>
+					{/if}
+				</div>
+				<div>
+					<input
+						name="cost"
+						type="number"
+						step="0.01"
+						placeholder="Amount Owed"
+						bind:value={$form.cost}
+						style={$errors.cost && 'border:2px solid rgb(232, 174, 174)'}
+					/>
+					{#if $errors.cost}
+						<p class="error-response">{$errors.cost}</p>
+					{/if}
+				</div>
+				<button>Submit</button>
+			</form>
+		</div>
+	</Modal>
+{/if}
 
 <style>
 	.debt-container {
--- src/lib/components/modal/PaymentModal.svelte
+++ src/lib/components/modal/PaymentModal.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { superForm } from 'sveltekit-superforms/client';
 	import type { PageData } from '../../../routes/dashboard/$types';
-	import type { TDebt } from '../../../routes/dashboard/+page.server';
 	import Modal from './Modal.svelte';
 	import toast from 'svelte-french-toast';
 
@@ -24,20 +23,22 @@
 	});
 </script>
 
-<Modal bind:showModal={payDebtModal}>
-	<form action="/dashboard?/paymentRequest" method="post" use:enhance>
-		<h3>Payment Request</h3>
-		<textarea
-			bind:value={$form.payment_details}
-			name="payment_details"
-			placeholder="Enter payment details..."
-		/>
-		{#if $errors.payment_details}
-			<p class="error-response">{$errors.payment_details}</p>
-		{/if}
-		<button> submit </button>
-	</form>
-</Modal>
+{#if payDebtModal}
+	<Modal bind:showModal={payDebtModal}>
+		<form action="/dashboard?/paymentRequest" method="post" use:enhance>
+			<h3>Payment Request</h3>
+			<textarea
+				bind:value={$form.payment_details}
+				name="payment_details"
+				placeholder="Enter payment details..."
+			/>
+			{#if $errors.payment_details}
+				<p class="error-response">{$errors.payment_details}</p>
+			{/if}
+			<button> submit </button>
+		</form>
+	</Modal>
+{/if}
 
 <style>
 	form {
