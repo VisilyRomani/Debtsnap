636648cdf587a785e20adb8cfc60e18857a54c0f
Visily <michael4161@hotmail.com>
2023-11-28 19:02:00 -0600

    svelte pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
-		VitePWA({
-			injectRegister: 'auto',
-			registerType: 'autoUpdate',
-			includeAssets: ['icon.png', 'favicon.svg'],
-			manifest: {
-				name: 'DebtSnap',
-				short_name: 'DebtSnap',
-				description:
-					'Streamline your finances with our debt tracking app - effortlessly manage IOUs and split expenses among friends.',
-				theme_color: '#f1fffa',
-				icons: [
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			}
-		}),
-		sveltekit()
+		sveltekit(),
+		SvelteKitPWA({
+			strategies: 'injectManifest',
+			srcDir: 'src',
+			filename: 'my-sw.js' // or `my-sw.ts`
+			/* other pwa options */
+		})
 	]
 });
