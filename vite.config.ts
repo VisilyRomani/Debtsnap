4e9ae920a3f06843bf70f4208fe1bf3918e3f806
Visily <michael4161@hotmail.com>
2023-11-28 19:12:19 -0600

    remove vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			strategies: 'injectManifest',
-			srcDir: 'src',
-			filename: 'my-sw.js' // or `my-sw.ts`
-			/* other pwa options */
-		})
-	]
+	plugins: [sveltekit()]
 });
