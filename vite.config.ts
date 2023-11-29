89e133acaad81d6a0e29ba4816c021128901119e
Visily <michael4161@hotmail.com>
2023-11-28 18:33:11 -0600

    pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,14 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
 	plugins: [
 		VitePWA({
-			registerType: 'autoUpdate',
-			devOptions: {
-				enabled: true
+			includeAssets: ['icon.png', 'favicon.svg'],
+			manifest: {
+				name: 'DebtSnap',
+				short_name: 'DebtSnap',
+				description:
+					'Streamline your finances with our debt tracking app - effortlessly manage IOUs and split expenses among friends.',
+				theme_color: '#f1fffa',
+				icons: [{ src: 'icon.png', sizes: '400x400', type: 'image/png' }]
 			}
 		}),
 		sveltekit()
