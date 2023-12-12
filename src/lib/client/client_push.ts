4bcfd2febd192808b068e1f91b2eb5155dbae5e1
Michael <michael4161@hotmail.com>
2023-12-11 19:07:42 -0600

    check for pushManager

--- src/lib/client/client_push.ts
+++ src/lib/client/client_push.ts
@@ -4,29 +4,31 @@ export const registerPush = async (user_id: string) => {
 	const registration = await navigator.serviceWorker.getRegistration();
 
 	try {
-		if (!registration?.pushManager) {
+		if (!registration) {
 			return;
+		} else if (!('pushManager' in registration)) {
+			return;
+		} else {
+			const permission = await registration.pushManager.permissionState({ userVisibleOnly: true });
+			if (registration?.pushManager && permission !== 'denied') {
+				const subscription = await registration.pushManager.subscribe({
+					userVisibleOnly: true,
+					applicationServerKey: PUBLIC_VAPID_KEY
+				});
+				const id = set_create_hardware();
+				if (subscription.endpoint) {
+					await fetch('/api/subscribe', {
+						method: 'POST',
+						body: JSON.stringify({ subscription, user_id, hardware_id: id })
+					});
+				} else {
+					console.log('missing endpoint');
+				}
+			} else {
+				console.error('Missing registration.pushManager');
+			}
 		}
 	} catch (e) {
 		console.log(e);
-	}
-
-	const permission = await registration?.pushManager.permissionState({ userVisibleOnly: true });
-	if (registration?.pushManager && permission !== 'denied') {
-		const subscription = await registration.pushManager.subscribe({
-			userVisibleOnly: true,
-			applicationServerKey: PUBLIC_VAPID_KEY
-		});
-		const id = set_create_hardware();
-		if (subscription.endpoint) {
-			await fetch('/api/subscribe', {
-				method: 'POST',
-				body: JSON.stringify({ subscription, user_id, hardware_id: id })
-			});
-		} else {
-			console.log('missing endpoint');
-		}
-	} else {
-		console.error('Missing registration.pushManager');
 	}
 };
