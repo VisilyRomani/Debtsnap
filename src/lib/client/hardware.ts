e441c2e2795b01b2d8b7f2754d366ba7ab06e372
Michael <michael4161@hotmail.com>
2023-12-11 18:43:29 -0600

    change to different id generator

--- src/lib/client/hardware.ts
+++ src/lib/client/hardware.ts
@@ -1,11 +1,13 @@
-import uniqid from 'uniqid';
+import ShortUniqueId from 'short-unique-id';
+
+const uid = new ShortUniqueId({ length: 10 });
 
 export const set_create_hardware = () => {
 	let hardware_id = localStorage.getItem('hardware_id');
 	if (hardware_id) {
 		return hardware_id;
 	} else {
-		const id = uniqid();
+		const id = uid.rnd();
 		localStorage.setItem('hardware_id', id);
 		console.log(id);
 		return id;
