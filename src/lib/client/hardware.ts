b5fdeffb7b0ffb50a5eeb8b6367aba884a10b5a6
Michael <michael4161@hotmail.com>
2023-12-11 18:31:14 -0600

    cant use cryptoUUId in insecure context

--- src/lib/client/hardware.ts
+++ src/lib/client/hardware.ts
@@ -3,9 +3,17 @@ export const set_create_hardware = () => {
 	if (hardware_id) {
 		return hardware_id;
 	} else {
-		const id = crypto.randomUUID();
+		const id = uuidv4();
 		localStorage.setItem('hardware_id', id);
 		console.log(id);
 		return id;
 	}
 };
+function uuidv4() {
+	return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+		(
+			parseInt(c) ^
+			(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c) / 4)))
+		).toString(16)
+	);
+}
